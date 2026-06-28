2c4bec9a68c82dbdd58d88f9530864e19c5c2768
kolavardhan <kolavardhan95@gmail.com>
2026-06-28 20:34:49 +0530

    fixes typo in authMiddleware.js

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,33 +1,21 @@
 const jwt = require('jsonwebtoken')
 
 const authMiddleware = (req, res, next) => {
-    const authHeaders = req.headers.authorization
+    const authHeader = req.headers.authorization
 
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(401).json({
             message: 'No token provided'
         })
     }
-    
-    const token = authHeaders.split(' ')[1]
 
-    const decode = jwt.verify(token, process.env.JWT_SECRET)
+    const token = authHeader.split(' ')[1]
 
-    req.user = decode
- 
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    req.user = decoded
+
     next()
 }
- 
+
 module.exports = authMiddleware
-// Frontend
-//      │
-//      │  Authorization: Bearer <JWT_TOKEN>
-//      ▼
-// authMiddleware
-//      │
-//      ├── Extract token
-//      ├── Verify token
-//      ├── Decode user id
-//      └── Attach it to req.user
-//      ▼
-// Todo Controller
